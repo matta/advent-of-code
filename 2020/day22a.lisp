de0bf163bc12ed19a64dedc573317db6fd636399
Matt Armstrong <gmatta@gmail.com>
2020-12-22 11:18:04 -0800

    Part Two done.

--- 2020/day22a.lisp
+++ 2020/day22a.lisp
@@ -23,6 +23,16 @@ Player 2:
 7
 10")
 
+(defparameter *example2-input*
+  "Player 1:
+43
+19
+
+Player 2:
+2
+29
+14")
+
 (defparameter *input*
   "Player 1:
 44
@@ -114,10 +124,49 @@ Player 2:
                (next-round card-a deck-a card-b deck-b)
                (next-round card-b deck-b card-a deck-a)))))))
 
-(defun play-combat (parsed)
+(defun play (game parsed)
   (destructuring-bind (player-a player-b) parsed
-    (combat (second player-a) (second player-b))))
+    (funcall game (second player-a) (second player-b))))
+
+(defun win-recursive-combat (player deck)
+  (list player (win-combat deck) deck))
+
+(defun recursive-combat (deck-a deck-b
+                         &optional (memory (make-hash-table :test #'equal)))
+  (let ((hands (list deck-a deck-b)))
+    (if (gethash hands memory)
+        (win-recursive-combat 'a deck-a)
+        (progn
+          (setf (gethash hands memory) t)
+          (cond
+            ((null deck-a) (win-recursive-combat 'b deck-b))
+            ((null deck-b) (win-recursive-combat 'a deck-a))
+            (t (let ((card-a (pop deck-a))
+                     (card-b (pop deck-b)))
+                 (if (if (and (>= (length deck-a) card-a)
+                              (>= (length deck-b) card-b))
+                         (eq 'a (first (recursive-combat
+                                        (subseq deck-a 0 card-a)
+                                        (subseq deck-b 0 card-b))))
+                         (> card-a card-b))
+                     (recursive-combat (append deck-a (list card-a card-b))
+                                       deck-b
+                                       memory)
+                     (recursive-combat deck-a
+                                       (append deck-b (list card-b card-a))
+                                       memory)))))))))
 
 (defun test ()
-  (assert (eql 306 (play-combat (parse-input *example-input*))))
-  (assert (eql 33473 (play-combat (parse-input *input*)))))
+  (assert (eql 306 (play #'combat (parse-input *example-input*))))
+  (assert (eql 33473 (play #'combat (parse-input *input*))))
+  (assert (equal '(B 291 (7 5 6 2 4 1 10 8 9 3))
+                 (play #'recursive-combat
+                       (parse-input *example-input*))))
+  (assert (equal '(A 105 (43 19))
+                 (play #'recursive-combat
+                       (parse-input *example2-input*))))
+  (assert (equal '(A 31793 (15 7 36 31 22 2 21 1 49 30 37 24 35 25 5 8 48 27
+                            33 19 43 26 14 3 18 13 50 46 40 20 47 39 28 4 38
+                            34 23 9 45 12 44 11 42 29 16 10 41 17 32 6))
+                 (play #'recursive-combat
+                       (parse-input *input*)))))
