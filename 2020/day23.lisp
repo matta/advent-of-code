c20bbfd40f7bf55ab147d354c102ac4fbd37ac35
Matt Armstrong <gmatta@gmail.com>
2020-12-24 11:59:41 -0800

    Day 23 Phase Two done.

--- 2020/day23.lisp
+++ 2020/day23.lisp
@@ -8,116 +8,108 @@
 (defpackage :day23 (:use #:common-lisp))
 (in-package :day23)
 
-(defparameter *example-input* "389125467")
 (defparameter *input* "463528179")
 
 (defun parse-input (str)
   (loop for i below (length str)
         collect (parse-integer str :start i :end (1+ i))))
 
-(defun make-circle (list)
-  (setf (cdr (last list)) list))
+(defun set-next (cups cup next-cup)
+  (setf (aref cups cup) next-cup))
 
-(defun length-circle (list)
-  "Like LENGTH, but works for circular lists."
-  (loop for curr on list
-        for rest = (rest curr)
-        count t into length
-        until (eq rest list)
-        finally (return length)))
+(defun get-next (cups cup)
+  (aref cups cup))
 
-(defun member-circle (item list)
-  "Like MEMBER, but works for circular lists."
-  (declare (type fixnum item))
-  (loop for curr on list
-        for rest = (rest curr)
-        count t into count
-        if (eql item (car curr))
-          do (return curr)
-        until (eq rest list)))
+(defun make-cups (initial-cups &optional (total-cups (length initial-cups)))
+  (let* ((max-initial (reduce #'max initial-cups))
+         (max-cup (+ max-initial (max 0 (- total-cups
+                                           (length initial-cups)))))
+         (cups (make-array (1+ max-cup)
+                           :initial-element 0
+                           :element-type 'integer))
+         (prev-cup (first initial-cups)))
+    (loop for cup in (rest initial-cups)
+          do (set-next cups prev-cup cup)
+          do (setf prev-cup cup))
+    (loop for cup from (1+ max-initial) to max-cup
+          do (set-next cups prev-cup cup)
+          do (setf prev-cup cup))
+    (set-next cups prev-cup (first initial-cups))
+    cups))
 
-(defun nremove-hand (list)
-  "Removes three elements from LIST starting from LIST's tail.
-LIST is destructively modified to splice the gap, and the
-three removed elements are returned as a proper list."
-  (let* ((hold (cdr list))
-         (tail (cddr hold)))
-    (setf (cdr list) (cdr tail))
-    (setf (cdr tail) nil)
-    hold))
+(defun extract-after-cup (cups cup n)
+  (loop with start = (get-next cups cup)
+        for i below n
+        for last = start then (get-next cups last)
+        finally (return (prog1 start
+                          (set-next cups cup (get-next cups last))
+                          (set-next cups last 0)))))
 
-(defun nsplice-after (from into)
-  "Splice FROM into (CDR INTO).  Both lists are destructively modified."
-  (setf (cdr (last from)) (rest into))
-  (setf (cdr into) from)
-  nil)
+(defun next-destination (cups cup)
+  (if (>= (decf cup) 1)
+      cup
+      (1- (length cups))))
 
-(defun next-destination (num min max)
-  "Given a cup number NUM, and the MIN and MAX allowed cup numbers,
-returns the next cup number."
-  (decf num)
-  (if (< num min)
-      max
-      num))
+(defun heldp (cups held cup)
+  (loop for c = held then (get-next cups c)
+        until (zerop c)
+        if (eql c cup)
+          do (return t)))
 
-(defun destination-cup (current min max)
-  "Returns the next destination cup, given the CURRENT cup and the MIN and MAX
-cup numbers.  Assumes that CURRENT is in a circular list."
-  (flet ((next (prev)
-           (next-destination prev min max)))
-    (loop for number = (next (car current)) then (next number)
-          for destination = (member-circle number current)
-          while (null destination)
-          finally (return destination))))
+(defun destination-cup (cups current held)
+  (flet ((next (cup) (next-destination cups cup)))
+    (loop for dest = (next current) then (next dest)
+          while (heldp cups held dest)
+          finally (return dest))))
 
-(defun order-after-cup1 (cups)
-  (loop for cup in (rest (member-circle 1 cups))
-        until (eql cup 1)
-        collect cup))
+(defun last-cup (cups head)
+  (loop for cup = head then next
+        for next = (get-next cups cup)
+        until (zerop next)
+        finally (return cup)))
 
-(defun game (cups moves)
-  (assert *print-circle*)
-  (loop with min = (reduce #'min cups)
-        with max = (reduce #'max cups)
-        for i from 0 below moves
-        for current = (make-circle (copy-list cups)) then (rest current)
-        for hold = (nremove-hand current)
-        for destination = (destination-cup current min max)
-        if (and (zerop (mod i 1000)) (> i 0))
-          do (format t "~&iteration ~S of ~S (~S)~%" i moves (* 1.0 (/ i moves)))
-        ;; do (format t "~&hold ~S~%" hold)
-        ;; do (format t "~&destination ~S~%" destination)
-        ;; do (format t "~&(car destination) ~S~%" (car destination))
-        ;; do (format t "~&current ~S (before splicing)~%" current)
-        do (nsplice-after hold destination)
-        ;; do (format t "~&current ~S (after splicing)~%" current)
-        ;; do (format t "~&(rest current) ~S (after splicing)~%" (rest current))
-        ;; do (format t "~%")
-        finally (return current)))
+(defun insert-cups (cups dest held)
+  (set-next cups (last-cup cups held) (get-next cups dest))
+  (set-next cups dest held))
 
-(defun game1 (cups moves)
-  (order-after-cup1 (game cups moves)))
+(defun cup-sublist (cups head &optional (n 100))
+  (loop for i below n
+        for cup = head then (get-next cups cup)
+        until (or (eql cup (first sublist)) (zerop cup))
+        collect cup into sublist
+        finally (return sublist)))
 
-;; (defun game2 (cups)
-;;   (let* ((one-million 1000000)
-;;          (one-million-cups
-;;            (append cups
-;;                    (loop for i from (1+ (length cups)) to one-million
-;;                          collect i)))
-;;          (result (game one-million-cups 10000000)))
-;;     (* (first result) (second result))))
+(defun game (input total-cups total-moves)
+  (assert (eql 9 (length input)))
+  (loop with initial-cups = (parse-input input)
+        with cups = (make-cups initial-cups total-cups)
+        for current = (first initial-cups) then (get-next cups current)
+        for i from 0 below total-moves
+        for held = (extract-after-cup cups current 3)
+        for dest = (destination-cup cups current held)
+        do (insert-cups cups dest held)
+        finally (return (rest (cup-sublist cups
+                                           1
+                                           (length initial-cups))))))
+
+(defun game1 (input total-moves)
+  (game input 9 total-moves))
+
+(defun game2 (input)
+  (let ((result (game input 1000000 10000000)))
+    (* (first result) (second result))))
 
 (defun test ()
   (assert (equal '(9 2 6 5 8 3 7 4)
-                 (game1 (parse-input *example-input*) 10)))
+                 (game1 "389125467" 10)))
   (assert (equal '(6 7 3 8 4 5 2 9)
-                 (game1 (parse-input *example-input*) 100)))
+                 (game1 "389125467" 100)))
 
   ;; This is the answer to Part One.
   (assert (equal '(5 2 9 3 7 8 4 6)
-                 (game1 (parse-input *input*) 100)))
+                 (game1 *input* 100)))
 
-  ;; (assert (equal -1
-  ;;                (game2 (parse-input *example-input*))))
-  )
+  (assert (equal 149245887792 (game2 "389125467")))
 
+  ;; This is the answer to Part Two.
+  (assert (equal 8456532414 (game2 *input*))))
